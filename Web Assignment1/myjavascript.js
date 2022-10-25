65096b6f25aae16fa6f51ddc629ba41ec3a38e2d
Muhammad Faizan Hameed <112592137+Faizium@users.noreply.github.com>
2022-10-25 17:19:23 -0400

    Add files via upload

--- Web Assignment1/myjavascript.js
+++ Web Assignment1/myjavascript.js
@@ -4,6 +4,10 @@ window.onload=function(){
   Addbtn.addEventListener("click", handleClick1);
   var Subbtn =  document.getElementById('subtract');
   Subbtn.addEventListener("click", handleClick2);
+  function2;
+  function3;
+  var details = document.getElementById("submit");
+  details.addEventListener("click",handleClick3);
 }
 
 function myFunction() {
@@ -24,3 +28,41 @@ function handleClick2() {
     console.log(val);
     document.getElementById('num').value = val;
 };
+function function2()
+{
+    
+    var val = Number(document.getElementById("format").value) ;
+    var Firstname = String(document.getElementById("firstname").value);
+    var Secondname = String(document.getElementById("secondname").value);
+    if (val == 1){
+        document.getElementById("namearea").innerText = Firstname+" "+Secondname ;
+    }
+    if (val == 2)
+    {
+       
+       document.getElementById("namearea").innerText = Secondname+" "+Firstname;
+       
+    }
+
+};
+function function3(){
+   var txt =String(document.getElementById("txt").value);
+   var len =  txt.split(" ").join("").length;
+   document.getElementById("txtlength").innerText = len ;
+}
+function handleClick3(){
+   
+    var txtlength = String( document.getElementById("name").value);
+    if (txtlength.length > 50 )
+    {
+        var nameError = document.getElementById("name");
+        nameError.classList.add("invalid");
+        nameError.innerText = "Name Feild cannot exceed 50 Characters";
+    }
+    if(txtlength.split(" ").join("").length == 0)
+    {
+        var nameError = document.getElementById("name");
+        nameError.classList.add("invalid");
+        nameError.innerText = "Name Feild cannot be Empty";   
+    }
+}
