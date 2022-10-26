51e96db33fdf7e766147de3deaa87f87df1ffbd0
Muhammad Faizan Hameed <112592137+Faizium@users.noreply.github.com>
2022-10-26 07:04:51 -0400

    Add files via upload

--- Web Assignment1/myjavascript.js
+++ Web Assignment1/myjavascript.js
@@ -6,8 +6,9 @@ window.onload=function(){
   Subbtn.addEventListener("click", handleClick2);
   function2;
   function3;
-  var details = document.getElementById("submit");
-  details.addEventListener("click",handleClick3);
+  //var details = document.getElementById("submit");
+  //details.addEventListener("click",handleClick3);
+  handleClick3;
 }
 
 function myFunction() {
@@ -49,20 +50,66 @@ function function3(){
    var txt =String(document.getElementById("txt").value);
    var len =  txt.split(" ").join("").length;
    document.getElementById("txtlength").innerText = len ;
-}
-function handleClick3(){
-   
+};
+function handleClick3()
+{
+    document.getElementById("nameerror").innerHTML = "";
+    document.getElementById("mailerror").innerHTML = "";
+    document.getElementById("doberror").innerHTML= "";
+    document.getElementById("Interest").innerHTML= "";
+    
+    
     var txtlength = String( document.getElementById("name").value);
+    var email = String( document.getElementById("email").value);
+    var date = String ( document.getElementById("dob").value);
+    var interest = document.getElementById("Interest").value;
+    
     if (txtlength.length > 50 )
     {
         var nameError = document.getElementById("name");
         nameError.classList.add("invalid");
-        nameError.innerText = "Name Feild cannot exceed 50 Characters";
+        nameError.value = "";
+        console.log("hello");
+        document.getElementById("nameerror").innerHTML = "Character Limit Exceeds "
     }
+
     if(txtlength.split(" ").join("").length == 0)
     {
         var nameError = document.getElementById("name");
         nameError.classList.add("invalid");
-        nameError.innerText = "Name Feild cannot be Empty";   
+        document.getElementById("nameerror").innerHTML = "Name cannot be Empty "
     }
-}
+
+    if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email))
+      {
+         document.getElementById("mailerror").innerHTML = "";
+         
+      }
+    else{
+        document.getElementById("mailerror").innerHTML = "Invalid Email Address";
+        var emailerror = document.getElementById("email");
+        emailerror.classList.add("invalid");
+      
+    }
+     if(email.length == 0)
+    {
+        var emailerror = document.getElementById("email");
+        emailerror.classList.add("invalid");
+        document.getElementById("mailerror").innerHTML = "Email cannot be Empty "
+    }
+   if (date.length== 0){
+        var emailerror = document.getElementById("dob");
+        emailerror.classList.add("invalid");
+        document.getElementById("doberror").innerHTML = "Date cannot be Empty " 
+   }    
+    if(date > "2009-10-26")
+    {
+        var emailerror = document.getElementById("dob");
+        emailerror.classList.add("invalid");
+        document.getElementById("doberror").innerHTML = "Hello Kid! You are too young for this"
+    }
+    if(interest == 0){
+
+        document.getElementById("interesterror").innerHTML = "Please select the value to proceed" ;
+    }
+};
